1bd8cebca5d7acef7c8e3f7de6b5dfa9d98720e6
gpalmarante <34726810+gpalmarante@users.noreply.github.com>
2021-03-28 19:00:47 -0300

    Add files via upload

---
+++ cadastrodeseries/Classes/serieRepositorio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cadastrodeseries.Classes;
+using cadastrodeseries.Interfaces;
+
+namespace cadastrodeseries.Classes
+{
+    public class serieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    
+    }
+}
---
+++ cadastrodeseries/Enum/Genero.cs
@@ -0,0 +1,16 @@
+﻿namespace cadastrodeseries
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura =2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+
+    }
+
+}
